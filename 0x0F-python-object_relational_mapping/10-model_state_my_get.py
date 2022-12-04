f0201c4c0b3420fae0b14bf4efb51c64c0f7b2d4
MUBIRI <mubirirobberts@gmail.com>
2022-12-04 12:11:29 -0800

    ORM Project

--- 0x0F-python-object_relational_mapping/10-model_state_my_get.py
+++ 0x0F-python-object_relational_mapping/10-model_state_my_get.py
@@ -1,27 +1,31 @@
 #!/usr/bin/python3
-'''
-a script that lists all State objects
-from the database hbtn_0e_6_usa
-'''
-
+"""
+a script that prints the State object with the name passed
+as argument from the database hbtn_0e_6_usa
+"""
 
 from sys import argv
-from sqlalchemy import create_engine
-from sqlalchemy.ext.declarative import declarative_base
-from sqlalchemy.orm import sessionmaker
 from model_state import Base, State
+from sqlalchemy import create_engine
+from sqlalchemy.orm import sessionmaker
+
 
 if __name__ == "__main__":
-    engine = create_engine(
-            'mysql+mysqldb://{}:{}@localhost/{}'.format(argv[1],
-                                                        argv[2],
-                                                        argv[3]))
+
+    # make engine for database
+    user = argv[1]
+    passwd = argv[2]
+    db = argv[3]
+    engine = create_engine('mysql+mysqldb://{}:{}@localhost/{}'.
+                           format(user, passwd, db), pool_pre_ping=True)
     Base.metadata.create_all(engine)
     Session = sessionmaker(bind=engine)
     session = Session()
+
+    # query python instance in database state id given state name
     state = session.query(State).filter_by(name=argv[4]).first()
-    if state is not None:
-            print(str(state.id))
+    if state:
+        print("{:d}".format(state.id))
     else:
         print("Not found")
     session.close()
