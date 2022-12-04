6f81d767ed104db4408c726049a148954e0b640b
MUBIRI <mubirirobberts@gmail.com>
2022-12-04 12:27:01 -0800

    Model city

--- 0x0F-python-object_relational_mapping/model_city.py
+++ 0x0F-python-object_relational_mapping/model_city.py
@@ -1,15 +1,21 @@
 #!/usr/bin/python3
+"""
+Defines class City
+"""
 
-
-from sqlalchemy import Column, Integer, String, ForeingnKey
+from sqlalchemy import Column, Integer, String, ForeignKey
+from sqlalchemy.ext.declarative import declarative_base
 from model_state import Base, State
+
+Base = declarative_base()
 
 
 class City(Base):
-    '''
-    class city
-    '''
-    __tablename__ = 'cities'
-    id = Column(Integer, primary_key=True)
+    """
+    Class City; instance of Base
+    Linked to MySQL table "city"
+    """
+    __tablename__ = "cities"
+    id = Column(Integer, nullable=False, primary_key=True)  # autoincrements
     name = Column(String(128), nullable=False)
-    state_id = Column(Integer, ForeignKey('states.id'))
+    state_id = Column(Integer, ForeignKey(State.id), nullable=False)
